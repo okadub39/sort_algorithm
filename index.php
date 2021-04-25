e2166433c426b6ac410702d496d09f38fafd60f0
okadub39 <web@nuevo.jp>
2021-04-26 00:18:37 +0900

    add bubble_sort

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>ソートアルゴリズムの一覧</title>
+	<meta charset="UTF-8">
+	<title>ソートアルゴリズムの一覧</title>
 </head>
 <body>
 	<h1>ソートアルゴリズムの一覧</h1>
 	<ul>
-		<li>バブルソート</li>
+		<li><a href="bubble_sort.php">バブルソート</a></li>
 		<li>コムソート</li>
 		<li>選択ソート</li>
 		<li>挿入ソート</li>
